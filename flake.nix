6e5c1eb6cf1fd080f3cadafd093c4639fc4fc90e
categoricalcat <catufuzgu@gmail.com>
2025-08-04 13:39:35 -0300

    Enhance NixOS configuration with WSL support and modular outputs
    - Added support for NixOS on WSL by including the 'nixos-wsl' input and configuring the default user and state version.
    - Restructured the outputs section to improve organization, separating configurations for 'nixos' and 'fufuwuqi'.
    - This update aims to streamline the configuration process and enhance compatibility with Windows Subsystem for Linux.

--- flake.nix
+++ flake.nix
@@ -4,26 +4,49 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.fufuwuqi = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.fufud = import ./users/home-fufud.nix;
-          home-manager.users.workd = import ./users/home-workd.nix;
-        }
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nixos-wsl,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            nixos-wsl.nixosModules.default
+            {
+              system.stateVersion = "25.05";
+              wsl.defaultUser = "wsl-nixos";
+              wsl.enable = true;
+            }
+          ];
+        };
+
+        fufuwuqi = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.fufud = import ./users/home-fufud.nix;
+              home-manager.users.workd = import ./users/home-workd.nix;
+            }
+          ];
+        };
+      };
     };
-  };
 }
