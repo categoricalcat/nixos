f453a587564fb7fc274519edeaa039c847bde138
categoricalcat <catufuzgu@gmail.com>
2025-08-17 20:47:09 -0300

    Refactor locale configuration in NixOS to enhance locale management
    - Restructured the locale settings in locale.nix to group default and additional locales under a single `i18n` attribute.
    - Added extra locales for English (UK and US) alongside the default Brazilian Portuguese locale.
    - Organized additional locale settings for clarity and maintainability.
    
    These changes aim to improve the organization of locale configurations and support multilingual environments in the NixOS setup.

--- modules/locale.nix
+++ modules/locale.nix
@@ -6,20 +6,25 @@
   # Time zone
   time.timeZone = "America/Sao_Paulo";
 
-  # Default locale
-  i18n.defaultLocale = "pt_BR.UTF-8";
+  i18n = {
+    defaultLocale = "pt_BR.UTF-8";
+    extraLocales = [
+      "pt_BR.UTF-8/UTF-8"
+      "en_GB.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+    ];
 
-  # Additional locale settings (Brazilian Portuguese for specific categories)
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-    LC_ALL = "pt_BR.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+      LC_ALL = "pt_BR.UTF-8";
+    };
   };
 }
