869dc315ebbcc5753db22636a7c4fef66065a871
categoricalcat <catufuzgu@gmail.com>
2025-10-07 17:36:40 -0300

    fix(configuration): update system packages and configurations for improved performance

--- hosts/fuyidong/configuration.nix
+++ hosts/fuyidong/configuration.nix
@@ -50,4 +50,34 @@ in
     enable = true;
     listenAddresses = [ ];
   };
+
+  # hardware.graphics = {
+  #   enable = false;
+  #   extraPackages = with pkgs; [
+  #     # For modern Intel GPUs (Arc and newer)
+  #     vpl-gpu-rt
+  #     intel-media-driver
+  #   ];
+  # };
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 80;
+  };
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd"; # Offers a good balance of compression speed and ratio
+    memoryPercent = 50; # This will reserve 50% of your 15GB (about 7.5GB) for zram
+  };
+
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "binarycache.example.com-1:dsafdafDFW123fdasfa123124FADSAD"
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
