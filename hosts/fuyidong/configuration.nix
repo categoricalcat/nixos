bd950d0c1b8e5275e1ad0a6180a905bfdda68358
categoricalcat <catufuzgu@gmail.com>
2025-10-12 19:16:18 -0300

    feat(configuration): enhance fuyidong host configuration; add build machines and update Nix settings with substituters and trusted keys

--- hosts/fuyidong/configuration.nix
+++ hosts/fuyidong/configuration.nix
@@ -30,16 +30,41 @@ in
     rocmSupport = false;
   };
 
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
+  nix = {
+    buildMachines = [
+      {
+        hostName = "fufud";
+        system = "x86_64-linux";
+        maxJobs = 15;
+        speedFactor = 3;
+        supportedFeatures = [
+          "nixos-test"
+          "benchmark"
+          "big-parallel"
+          "kvm"
+        ];
+        sshUser = "fufud";
+        sshKey = "/home/fufud/.ssh/id_ed25519";
+      }
     ];
-    download-buffer-size = 1024 * 1024 * 1024 * 10;
-    # substituters = [
-    #   "https://nix-community.cachix.org"
-    #   "https://cache.nixos.org/"
-    # ];
+
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      download-buffer-size = 1024 * 1024 * 1024 * 10;
+
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://nixos-rocm.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nixos-rocm.cachix.org-1:VEpsf7pRIijjd8csKjFNBGzkBqOmw8H9PRmgAq14LnE"
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 
   nixowos.enable = true;
