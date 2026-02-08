7fe0bf907df6dd8789ac1eb6ba9dec3bd4e620e6
categoricalcat <catufuzgu@gmail.com>
2026-02-08 19:47:09 -0300

    chore: update dms

--- modules/desktop/dms.nix
+++ modules/desktop/dms.nix
@@ -1,23 +1,25 @@
 {
   config,
   lib,
-  inputs,
   pkgs,
   ...
 }:
 
 {
   imports = [
-    inputs.dms.nixosModules.greeter
+    # inputs.dms.nixosModules.greeter
   ];
 
   config = lib.mkIf (config.desktop.environment == "niri") {
     environment.systemPackages = with pkgs; [
       polkit_gnome
     ];
-    programs.dank-material-shell.greeter = {
-      enable = false;
-      compositor.name = "niri";
-    };
+
+    services.accounts-daemon.enable = true;
+
+    #programs.dank-material-shell.greeter = {
+    #  enable = false;
+    #  compositor.name = "niri";
+    #};
   };
 }
