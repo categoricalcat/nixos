4759a1d02d0d10cdf4f58da44c8751d24627db5a
categoricalcat <catufuzgu@gmail.com>
2026-02-20 20:28:19 -0300

    feat: flake update + rust in devshell

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -4,8 +4,22 @@ _: {
     {
       devShells.default = pkgs.mkShell {
         inherit (config.pre-commit.devShell) shellHook;
+
         # packages = with pkgs; [ udev ];
+
+        strictDeps = true;
+        nativeBuildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+        ];
+
+        RUST_SRC_PATH = with pkgs; "${rustPlatform.rustLibSrc}";
       };
+
       packages.default = config.devShells.default;
+
     };
 }
