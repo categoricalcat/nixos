7e13e6e302c2d66912cf8768a40077ffe40dffa5
categoricalcat <catufuzgu@gmail.com>
2026-05-30 13:33:49 -0300

    ci: add develop branch, restrict jobs to owner, and fix public key format

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -40,7 +40,7 @@ in
         "https://cache.nixos.org/"
       ];
       trusted-public-keys = [
-        "yifuwuqi-cache:k3diDzo5ydrNjlrbZexjTtQxK67HEJYZqGA4r3c7azM=%"
+        "yifuwuqi-cache:k3diDzo5ydrNjlrbZexjTtQxK67HEJYZqGA4r3c7azM="
         "nixos-rocm.cachix.org-1:VEpsf7pRIijjd8csKjFNBGzkBqOmw8H9PRmgAq14LnE"
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
