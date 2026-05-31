91bc5f6914eb87e1b2e68c88319b25deb1ab809f
categoricalcat <catufuzgu@gmail.com>
2026-05-31 09:47:34 -0300

    fix binaryCachesPath

--- modules/hercules-ci.nix
+++ modules/hercules-ci.nix
@@ -1,10 +1,11 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   services.hercules-ci-agent = {
     enable = true;
     settings = {
       clusterJoinTokenPath = config.sops.secrets."tokens/hercules-ci-cluster-join-token".path;
+      binaryCachesPath = pkgs.writeText "binary-caches.json" "{}";
     };
   };
 
